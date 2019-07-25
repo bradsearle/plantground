61ea3b88dfb8ae3b2df6a4ffe3d590b0cc0bb313
Brad Searle <basearle@gmail.com>
2019-07-25 17:49:44 -0500

    made some test image sass adjustments

--- dev.php
+++ dev.php
@@ -1,7 +1,14 @@
 <?php /* Template Name: Homepage development */ get_header(); ?>
 
-
 <?php include get_theme_file_path( 'head.php' );?>
+
+
+<div class="welcome">
+<img class="welcome__img" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/hero-daisey.jpg"/>
+</div>
+
+
+
 <?php the_content(); ?>
 
 
