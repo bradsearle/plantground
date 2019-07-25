5691c5797c52efae1a6da078fac9630c93742434
Brad Searle <basearle@gmail.com>
2019-07-25 17:29:37 -0500

    style adjustments

--- dev.php
+++ dev.php
@@ -1,46 +1,9 @@
 <?php /* Template Name: Homepage development */ get_header(); ?>
 
-	<main role="main">
-		<!-- section -->
-		<section>
 
-			
+<?php include get_theme_file_path( 'head.php' );?>
+<?php the_content(); ?>
 
-		<?php if (have_posts()): while (have_posts()) : the_post(); ?>
-
-			<!-- article -->
-			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-				<?php the_content(); ?>
-
-				<?php comments_template( '', true ); // Remove if you don't want comments ?>
-
-				<br class="clear">
-
-				<?php edit_post_link(); ?>
-
-			</article>
-			<!-- /article -->
-			
-			<p>testing..../</p>
-
-		<?php endwhile; ?>
-
-		<?php else: ?>
-
-			<!-- article -->
-			<article>
-
-				<h2><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h2>
-
-			</article>
-			<!-- /article -->
-
-		<?php endif; ?>
-
-		</section>
-		<!-- /section -->
-	</main>
 
 <?php get_sidebar(); ?>
 
