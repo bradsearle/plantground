8191230a67ec112d68af15ad13c3ad161ccd2dcc
Brad Searle <basearle@gmail.com>
2019-08-26 17:26:44 -0500

    edited footer and hp template. removed wrapper

--- footer.php
+++ footer.php
@@ -1,30 +1,33 @@
 			<!-- footer -->
 			<footer class="footer" role="contentinfo">
-testing...
-				<!-- copyright -->
-				<p class="copyright">
-					&copy; <?php echo date('Y'); ?> Copyright 
-				
-				</p>
-				<!-- /copyright -->
+
+				footer content goes here
 
 			</footer>
 			<!-- /footer -->
 
-		</div>
-		<!-- /wrapper -->
+			<!-- </div> -->
+			<!-- /wrapper -->
 
-		<?php wp_footer(); ?>
+			<?php wp_footer(); ?>
 
-		<!-- analytics -->
-		<script>
-		(function(f,i,r,e,s,h,l){i['GoogleAnalyticsObject']=s;f[s]=f[s]||function(){
-		(f[s].q=f[s].q||[]).push(arguments)},f[s].l=1*new Date();h=i.createElement(r),
-		l=i.getElementsByTagName(r)[0];h.async=1;h.src=e;l.parentNode.insertBefore(h,l)
-		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-		ga('create', 'UA-28390782-2', 'plantground.com');
-		ga('send', 'pageview');
-		</script>
+			<!-- analytics -->
+			<script>
+				(function (f, i, r, e, s, h, l) {
+					i['GoogleAnalyticsObject'] = s;
+					f[s] = f[s] || function () {
+						(f[s].q = f[s].q || []).push(arguments)
+					}, f[s].l = 1 * new Date();
+					h = i.createElement(r),
+						l = i.getElementsByTagName(r)[0];
+					h.async = 1;
+					h.src = e;
+					l.parentNode.insertBefore(h, l)
+				})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+				ga('create', 'UA-28390782-2', 'plantground.com');
+				ga('send', 'pageview');
+			</script>
 
-	</body>
-</html>
+			</body>
+
+			</html>
