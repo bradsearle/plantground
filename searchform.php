c267c8cc12d7998f88236ae6e5962fb16639c3df
Brad Searle <basearle@gmail.com>
2020-01-29 12:23:33 -0600

    creaded a basic toggle for search
    will come back and add design

--- searchform.php
+++ searchform.php
@@ -1,6 +1,20 @@
-<!-- search -->
-<form class="search" method="get" action="<?php echo home_url(); ?>" role="search">
-	<input class="search-input" type="search" name="s" placeholder="<?php _e( 'yo, type and hit enter.', 'html5blank' ); ?>">
-	<button class="search-submit" type="submit" role="button"><?php _e( 'Search', 'html5blank' ); ?></button>
-</form>
-<!-- /search -->
+<style>
+	.test {
+		display: none;
+	}
+</style>
+
+
+<p>
+	<a class="toggle" href="#example">search</a>
+</p>
+
+<div class="test" id="example">
+	<form class="search" method="get" action="<?php echo home_url(); ?>" role="search">
+		<input class="search-input" type="search" name="s" placeholder="<?php _e( 'search', 'html5blank' ); ?>">
+		<button class="search-submit" type="submit" role="button"><?php _e( 'Search', 'html5blank' ); ?></button>
+	</form>
+
+	<a class="toggle" href="#example">X</a>
+
+</div>
