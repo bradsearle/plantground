1e91c28e9374291a736cc26eeb58bce8be7a20b1
Brad.Searle <basearle@gmail.com>
2020-02-26 00:22:06 -0600

    testing cart functionality

--- head.php
+++ head.php
@@ -9,9 +9,17 @@
 
 
 	<nav class="nav" role="navigation">
+	<?php global $woocommerce; ?>
+ <a class="bas" href="<?php echo $woocommerce->cart->get_cart_url(); ?>"
+title="<?php _e('Cart View', 'woothemes'); ?>">
+<?php echo sprintf(_n('%d item', '%d items', $woocommerce->cart->cart_contents_count, 'woothemes'),
+ $woocommerce->cart->cart_contents_count);?>  -
+<?php echo $woocommerce->cart->get_cart_total(); ?>
 	<i class="material-icons">
 shopping_cart
 </i>
+
+
 		<?php html5blank_nav(); ?>
 	</nav>
 	
