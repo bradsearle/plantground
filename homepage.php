0042dbbcc5c27cf842e8b3d755d4e53ff456b57d
Brad.Searle <basearle@gmail.com>
2020-06-27 02:54:34 -0500

    styles temp homepage

--- homepage.php
+++ homepage.php
@@ -2,7 +2,7 @@
 
 
 <?php get_sidebar(); ?>
-
+  <?php echo get_template_directory_uri(); ?>
 <?php get_footer(); ?>
 */ ?>
 
@@ -27,5 +27,10 @@
 
 
   <?php endif; ?>
+
+<div class="hp-img">
+  <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/bishop-cactus.png" />
+  in development
+</div>
 
 </section>
